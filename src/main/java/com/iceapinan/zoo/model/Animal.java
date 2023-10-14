d6db9d4670063b1e9ce0332984a0001ae302f517
Sopanat Suradach <sopanus2014@gmail.com>
2023-10-14 00:24:28 +0000

    Refactor Cage and Animal classes for better encapsulation and readability

--- src/main/java/com/iceapinan/zoo/model/Animal.java
+++ src/main/java/com/iceapinan/zoo/model/Animal.java
@@ -5,22 +5,63 @@ import java.util.List;
 import javax.persistence.*;
 
 @Entity
-public class Animal {
+public abstract class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+    private Long id;
 
     @Column(nullable = false)
-    public String name;
-    public String family;
-    public String type; // Herbivore or Carnivore
+    private String name;
+    private String family;
+    private String type; // Herbivore or Carnivore
+
+    public Animal() {}
+
+    public Animal(String name, String family, String type) {
+        this.name = name;
+        this.family = family;
+        this.type = type;
+    }
 
     public List<String> getInfo() {
-        List<String> listInfo = new ArrayList<String>();
+        List<String> listInfo = new ArrayList<>();
         listInfo.add("Name: " + name);
         listInfo.add("Family: " + family);
         listInfo.add("Type: " + type);
         return listInfo;
     }
-    public String say() { return ""; };
+
+    public abstract String say();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
